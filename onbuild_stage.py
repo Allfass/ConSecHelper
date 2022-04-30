6412ecc1bf7ebcae2fb6ef8b93a93892f90ca220
allfass <sh98allfass@gmail.com>
2022-04-30 15:51:01 +0500

    #8 - change behavior func

--- onbuild_stage.py
+++ onbuild_stage.py
@@ -1,4 +1,11 @@
 def onbuild(string, dockerfile):
-    # Используем непревилигированного пользователя
-    dockerfile.write(string[0]+" RUN groupadd -r docker")
-    dockerfile.write(string[0]+" RUN useradd -r -g docker docker")
+    # проверка на наличие инструкции
+    if "useradd" in string[1]:
+        # выполняем копирование
+        dockerfile.write(string[0], ' ', string[1])
+    else:
+        # выполняем копирование
+        dockerfile.write(string[0], ' ', string[1])
+        # Добавляем непревилигированного пользователя
+        dockerfile.write(string[0]+" RUN groupadd -r docker")
+        dockerfile.write(string[0]+" RUN useradd -r -g docker docker")
