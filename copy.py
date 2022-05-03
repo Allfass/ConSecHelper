d86df79dd348c5425f2a02d1b28020ff5afec94d
allfass <sh98allfass@gmail.com>
2022-05-03 23:50:48 +0500

    #10 add comment into copy stage

--- copy.py
+++ copy.py
@@ -1,2 +1,4 @@
+# Функция копирует инструкцию в новый dockerfile
+# Используется для инструкций, автоматическая обработка которых - затруднительна или невозможна
 def copy_stage(string, dockerfile):
     dockerfile.write(string[0], ' ', string[1])
